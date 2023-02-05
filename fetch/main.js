aaa62acd0b3c936886280f11c44a8b2848386ee1
Daniel Seong <dseong0224@gmail.com>
2023-02-05 06:27:30 +0000

    completed fetch

---
+++ fetch/main.js
@@ -0,0 +1,7 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(res => res.json())
+  .then(data => console.log(data));
